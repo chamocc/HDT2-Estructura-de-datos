92c001aaec812b11d8cc7712a21ed76918fc9d8c
chamocc <chamocc@gmail.com>
2014-07-19 13:40:30 -0600

    encabezados

--- Calculadora/src/StackVector.java
+++ Calculadora/src/StackVector.java
@@ -1,12 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- * 
- * @author Ed. Chamo
+ * @author Daniel Gerendas 13158
+ * @author Edgar Chamo 13083
+ * @since 18/7/2014
  */
 import java.util.*;
 public class StackVector <E> implements Stack <E> {
